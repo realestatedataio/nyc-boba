df6f5a9e69a2774db6e35ac7bb3c29a8e5002168
Josh Chamberlain <josh@cjmlabs.com>
2024-03-07 16:38:29 -0800

    download implementation of pluto, mappluto, pad, snd

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-import * as PadDownloader from "./lib/downloader/pad/index.js";
-import * as SndDownloader from "./lib/downloader/snd/index.js";
-import * as PlutoDownloader from "./lib/downloader/pluto/index.js";
-import * as MapPlutoDownloader from "./lib/downloader/mappluto/index.js";
+import { default as PadDownloader } from "./lib/downloader/pad/index.js";
+import { default as SndDownloader } from "./lib/downloader/snd/index.js";
+import { default as PlutoDownloader } from "./lib/downloader/pluto/index.js";
+import { default as MapPlutoDownloader } from "./lib/downloader/mappluto/index.js";
 
 
 
--- util/downloaders.js
+++ util/downloaders.js
@@ -1,8 +1,27 @@
-import * as RediNycBoba from "../index.js";
+import { default as RediNycBoba } from "../index.js";
 
 
 
 
 const Run = async () =>
 {
+    console.log(RediNycBoba);
+
+    let padDownloader = new RediNycBoba.PadDownloader();
+    let padFile = await padDownloader.Run();
+    console.log("PAD downloaded to: " + padFile);
+
+    let plutoDownloader = new RediNycBoba.PlutoDownloader();
+    let plutoFile = await plutoDownloader.Run();
+    console.log("PLUTO downloaded to: " + plutoFile);
+
+    let mapPlutoDownloader = new RediNycBoba.MapPlutoDownloader();
+    let mapPlutoFile = await mapPlutoDownloader.Run();
+    console.log("MAP PLUTO downloaded to: " + mapPlutoFile);
+
+    let sndDownloader = new RediNycBoba.SndDownloader();
+    let sndFile = await sndDownloader.Run();
+    console.log("SND downloaded to: " + sndFile);
 };
+
+Run();
